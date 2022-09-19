79f07e487aff833c8674c5286883bf2e02f84167
SemenchenkoA <semenchenko_aleksey@mail.ru>
2022-09-20 00:53:24 +0300

    add test combination compare

--- src/com/sa/test/PokerSortTest.java
+++ src/com/sa/test/PokerSortTest.java
@@ -37,25 +37,25 @@ public class PokerSortTest extends Assert {
 
         Collections.sort(pokerHands, new PokerHandComparator());
 
-        assertEquals(pokerHands.get(0).getHand().equals("JH TH KH QH AH"), true);
-        assertEquals(pokerHands.get(1).getHand().equals("8H 9H TH JH QH"), true);
-        assertEquals(pokerHands.get(2).getHand().equals("AH AC AH AC TS"), true);
-        assertEquals(pokerHands.get(3).getHand().equals("8H 8C 8S JH 8D"), true);
-        assertEquals(pokerHands.get(4).getHand().equals("8H 8C 8S TH 8D"), true);
-        assertEquals(pokerHands.get(5).getHand().equals("8H 8C 8S 3H 8D"), true);
-        assertEquals(pokerHands.get(6).getHand().equals("8H 8C 8S 2H 8D"), true);
-        assertEquals(pokerHands.get(7).getHand().equals("8H 8C TH TC TS"), true);
-        assertEquals(pokerHands.get(8).getHand().equals("8H TH 6H 5H 2H"), true);
-        assertEquals(pokerHands.get(9).getHand().equals("8H 9C TH JH QH"), true);
-        assertEquals(pokerHands.get(10).getHand().equals("8H 9C TH JH 7C"), true);
-        assertEquals(pokerHands.get(11).getHand().equals("8H 8C TH TC QH"), true);
-        assertEquals(pokerHands.get(12).getHand().equals("AH AC 2H JH QH"), true);
-        assertEquals(pokerHands.get(13).getHand().equals("KH KC TH 3H QH"), true);
-        assertEquals(pokerHands.get(14).getHand().equals("KH KC TH 3H JH"), true);
-        assertEquals(pokerHands.get(15).getHand().equals("8H 8C 6H 5H 2H"), true);
-        assertEquals(pokerHands.get(16).getHand().equals("JH KC TH 3H QH"), true);
-        assertEquals(pokerHands.get(17).getHand().equals("8H TC 6H 5D 2C"), true);
-        assertEquals(pokerHands.get(18).getHand().equals("3H TC 6H 5D 2C"), true);
+        assertTrue(pokerHands.get(0).getHand().equals("JH TH KH QH AH"));
+        assertTrue(pokerHands.get(1).getHand().equals("8H 9H TH JH QH"));
+        assertTrue(pokerHands.get(2).getHand().equals("AH AC AH AC TS"));
+        assertTrue(pokerHands.get(3).getHand().equals("8H 8C 8S JH 8D"));
+        assertTrue(pokerHands.get(4).getHand().equals("8H 8C 8S TH 8D"));
+        assertTrue(pokerHands.get(5).getHand().equals("8H 8C 8S 3H 8D"));
+        assertTrue(pokerHands.get(6).getHand().equals("8H 8C 8S 2H 8D"));
+        assertTrue(pokerHands.get(7).getHand().equals("8H 8C TH TC TS"));
+        assertTrue(pokerHands.get(8).getHand().equals("8H TH 6H 5H 2H"));
+        assertTrue(pokerHands.get(9).getHand().equals("8H 9C TH JH QH"));
+        assertTrue(pokerHands.get(10).getHand().equals("8H 9C TH JH 7C"));
+        assertTrue(pokerHands.get(11).getHand().equals("8H 8C TH TC QH"));
+        assertTrue(pokerHands.get(12).getHand().equals("AH AC 2H JH QH"));
+        assertTrue(pokerHands.get(13).getHand().equals("KH KC TH 3H QH"));
+        assertTrue(pokerHands.get(14).getHand().equals("KH KC TH 3H JH"));
+        assertTrue(pokerHands.get(15).getHand().equals("8H 8C 6H 5H 2H"));
+        assertTrue(pokerHands.get(16).getHand().equals("JH KC TH 3H QH"));
+        assertTrue(pokerHands.get(17).getHand().equals("8H TC 6H 5D 2C"));
+        assertTrue(pokerHands.get(18).getHand().equals("3H TC 6H 5D 2C"));
 
         System.out.println(pokerHands);
     }
@@ -81,5 +81,23 @@ public class PokerSortTest extends Assert {
         assertEquals(Combination.detectCombination(new PokerHand("8H 8C 8S JH 8D")), Combination.PokerCombination.FOUR_OF_KIND);
         assertEquals(Combination.detectCombination(new PokerHand("8H 8C 8S TH 8D")), Combination.PokerCombination.FOUR_OF_KIND);
         assertEquals(Combination.detectCombination(new PokerHand("8H 8C 8S 3H 8D")), Combination.PokerCombination.FOUR_OF_KIND);
+    }
+
+    @Test
+    public void testCompareCombination(){
+        double powerHand1 = Combination.getPower(new PokerHand("3H TC 6H 5D 2C"));
+        double powerHand2 = Combination.getPower(new PokerHand("KH KC TH 3H QH"));
+
+        assertTrue(powerHand1 < powerHand2);
+
+        System.out.println("power hand1 = %.13f, power hand2 = %.13f".formatted(powerHand1, powerHand2));
+
+        powerHand1 = Combination.getPower(new PokerHand("8H 8C 8S 2H 8D"));
+        powerHand2 = Combination.getPower(new PokerHand("JH TH KH QH AH"));
+
+        assertTrue(powerHand1 < powerHand2);
+
+        System.out.println("power hand1 = %.13f, power hand2 = %.13f".formatted(powerHand1, powerHand2));
+
     }
 }
